ebf5d92ada4d10835d4f545017f92b2952f2e755
SohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2016-04-26 10:12:10 -0700

    26 / 4 / 2016

--- DES.py
+++ DES.py
@@ -25,7 +25,7 @@ class DES ():
            
      
     def   __pc1method__(self):
-        print ("pc1method")
+        """pc1method"""
         #print (self.listkey)
         #to make correct permutation on list key we need add bit at the begining each row 
         self.listkey.insert(0,'0')
@@ -355,8 +355,126 @@ class DES ():
             self.ResultXOR.append (NowRes)
             i+=1
 
-        print (len ((self.ResultXOR[2])))
+        #print ((self.ResultXOR[2]))
+        self.BreakResultofXOR()
+    def  BreakResultofXOR(self) :
+        """Break E(R[i-1]) xor K[i] into eight 6-bit blocks.
+         Bits 1-6 are B[1], bits 7-12 are B[2], and so on with bits 43-48 being B[8]."""
+        self.B = []
+        j=0
+        while (j<=16):
+            self.CLB = []
+            self.CLB .append (self.ResultXOR[j][:6])
+            self.CLB .append ( self.ResultXOR[j][6:12])
+            self.CLB .append ( self.ResultXOR[j][12:18])
+            self.CLB .append ( self.ResultXOR[j][18:24])
+            self.CLB .append ( self.ResultXOR[j][24:30])
+            self.CLB .append ( self.ResultXOR[j][30:36])
+            self.CLB .append ( self.ResultXOR[j][36:42])
+            self.CLB .append ( self.ResultXOR[j][42:])
+            #print (self.CLB)
+            self.B.append (self.CLB)
+            j+=1
+        #print (self.B)
+        self.__Substitution()
+
+    def __Substitution (self):
+        """Substitute the values found in the S-boxes for all B[j]. Start with j = 1.
+           All values in the S-boxes should be considered 4 bits wide.
+          Take the 1st and 6th bits of B[j] together as a 2-bit value (call it m)
+          indicating the row in S[j] to look in for the substitution.
+          Take the 2nd through 5th bits of B[j] together as a 4-bit value (call it n)
+           indicating the column in S[j] to find the substitution."""
+        __sbox = [
+		# S1
+                                   [
+		 [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+		 [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+		 [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+		 [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+                                    ],
+		# S2
+                                   [
+		[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+		 [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+		 [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+		 [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+                                    ],
+		# S3
+                                   [
+		[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+		 [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+		 [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+		 [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+                                     ],
+		# S4
+                                   [
+		[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+		 [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+		 [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+		 [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+                                    ],
+		# S5
+                                   [
+		[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+		 [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+		 [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+		 [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+                                     ],
+		# S6
+                                   [
+		[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+		 [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+		 [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+		 [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+                                     ],
+
+		# S7
+                                   [
+		[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+		 [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+		 [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+		 [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+                                     ],
+
+		# S8
+                                   [
+		[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+		 [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+		[ 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+		 [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+                                    ]
+	]
+        print ( __sbox [0][0][5])
+
+        print (self.B[0])
+        m = self.B[0][0][0] + self.B[0][0][5]
+        n = self.B[0][0][1:5]
+        #print (m)
+        #convert m to integer value
+        #print (self.B[0][0][5])
+        mvalue = ( 2* self.B[0][0][0] ) + (1* self.B[0][0][5])
+        print (mvalue )
+        #convert n to integer value
+        #print (n)
+        #print (n[3])
+        nvalue = n[3]*1
+        nvalue = nvalue + (n[2] *2)
+        nvalue = nvalue + (n[1] *4)
+        nvalue = nvalue + (n[0] *8 )
+        print (nvalue )
+        
+        
+        
+        
+         
+
+
+
+        
             
+            
+                
         
   
                 
